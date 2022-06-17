a5d773fb9d66373ff6886b4a6ff0282133cc26b5
Diane Bamanya <dianebamanya@Dianes-MBP.ht.home>
2022-06-17 10:53:42 -0400

    project setup with Tailwind

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+// import './App.css';
+import Cards from './components/Cards/Cards';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Hero/>
+     <Cards/>
+     {/* <About/> */}
+     {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
