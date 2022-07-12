8b8a48ed686e0d8edc9438ed190d14d4ef7fa591
Diane Bamanya <dianebamanya@Dianes-MacBook-Pro.local>
2022-07-12 07:51:43 -0400

    Added use-effect and axios WIP

--- client/src/components/pages/Home/Drinks.js
+++ client/src/components/pages/Home/Drinks.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { useEffect } from "react";
 
 const Drinks = () => {
-  return <div>Drinks lorem</div>;
+  const [newdrinks, setnewdrinks] = useState([]);
+  console.log(newdrinks);
+
+  useEffect(() => {
+    async function fetchData() {
+      await axios
+        .get("https://jsonplaceholder.typicode.com/users/")
+        .then((res) => {
+          //return res
+          setnewdrinks(res.data);
+          console.log(newdrinks);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      Drinks lorem ipsum
+      <div>skdjd</div>
+    </div>
+  );
 };
 
 export default Drinks;
